bd38292b9bac7eacf73b3f5f5072a84e2be19108
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2021-12-28 17:09:00 -0600

    Changed font and name of blog

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -3,22 +3,22 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 const register = (req,res) => {
-  return res.status(400).json({"message": "No new blog authors are allowed to register"});
-  // if(!req.body.email || !req.body.password) {
-  //   return res.status(400).json({"message": "All fields required"});
-  // }
-  // const user = new User();
-  // user.email = req.body.email;
-  // user.setPassword(req.body.password);
-  // user.save((err) => {
-  //   if(err) {
-  //     console.log(err);
-  //     res.status(404).json(err);
-  //   } else {
-  //     const token = user.generateJwt();
-  //     res.status(200).json({token});
-  //   }
-  // })
+  // return res.status(400).json({"message": "No new blog authors are allowed to register"});
+  if(!req.body.email || !req.body.password) {
+    return res.status(400).json({"message": "All fields required"});
+  }
+  const user = new User();
+  user.email = req.body.email;
+  user.setPassword(req.body.password);
+  user.save((err) => {
+    if(err) {
+      console.log(err);
+      res.status(404).json(err);
+    } else {
+      const token = user.generateJwt();
+      res.status(200).json({token});
+    }
+  })
 };
 
 const login = (req,res) => {
